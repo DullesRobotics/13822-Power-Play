742ba998297b8d416633c9bfc2b522dcaae01634
Coder5761 <abhijit.sudharsan@outlook.com>
2023-01-17 15:04:43 -0600

    Copied Programs from Big Red

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fenrir/OpModes/TeleOpModes/MasterTeleOp.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Tyre.OpModes.TeleOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.RobotManager.MecanumDriveTrain;
+import org.firstinspires.ftc.teamcode.RobotManager.StandardDriveTrain;
+import org.firstinspires.ftc.teamcode.Tyre.Configurator;
+//import org.firstinspires.ftc.teamcode.Tyre.ControlCenterTeleOp;
+
+@TeleOp
+public class MasterTeleOp extends LinearOpMode {
+    private MecanumDriveTrain baseRobot;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        baseRobot = new MecanumDriveTrain(this);
+        baseRobot.addHardware(Configurator.getHardware(baseRobot));
+
+        waitForStart();
+
+        baseRobot.driveWithController(baseRobot.ctrl1());
+
+
+        while (opModeIsActive())
+            baseRobot.getLogger().updateLog();
+
+        baseRobot.stopAllThreads();
+    }
+}
