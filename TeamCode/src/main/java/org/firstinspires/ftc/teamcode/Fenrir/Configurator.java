fbac9e2f5a4c3668e36f23e5e8e051a08a6f14f6
IzackW <izackwaczko@gmail.com>
2023-01-19 20:15:43 -0600

    Config exists now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fenrir/Configurator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fenrir/Configurator.java
@@ -2,6 +2,15 @@ package org.firstinspires.ftc.teamcode.Fenrir;
 
 import org.firstinspires.ftc.teamcode.Hardware.HardwareComponent;
 import org.firstinspires.ftc.teamcode.RobotManager.Robot;
+import org.firstinspires.ftc.teamcode.Hardware.Motor.DrivetrainMotor;
+import org.firstinspires.ftc.teamcode.Hardware.Motor.Motor;
+import org.firstinspires.ftc.teamcode.Hardware.Motor.MotorConfiguration;
+import org.firstinspires.ftc.teamcode.Hardware.Motor.MotorType;
+import org.firstinspires.ftc.teamcode.Hardware.Servo;
+import org.firstinspires.ftc.teamcode.Hardware.USBWebcam;
+import org.firstinspires.ftc.teamcode.Libraries.IMU;
+
+import static org.firstinspires.ftc.teamcode.Hardware.ComponentArea.*;
 
 public class Configurator {
 
@@ -11,8 +20,44 @@ public class Configurator {
      * @return The main hardware list for the robot
      */
     public static HardwareComponent[] getHardware(Robot robot){
-        return new HardwareComponent[]{
 
-        };
+        HardwareComponent[] driveTrainMotors = getDriveTrainMotors(robot);
+        Motor carouselMotor = new Motor(robot, "CAR" ,CAROUSEL ,false );
+
+        return(new HardwareComponent[]{
+                driveTrainMotors[0],
+                driveTrainMotors[1],
+                driveTrainMotors[2],
+                driveTrainMotors[3],
+                new Motor(robot, "LIFT", LIFT, false),
+                new Motor(robot, "IN", INTAKE, false),
+                new IMU(robot, "IMU"),
+                new Servo(robot, "OT", OUTTAKE),
+                new Servo(robot, "ARM", CLAW),
+                new USBWebcam(robot, "Webcam"),
+                //  new ColorSensor(robot, "CS", INTAKE),
+                carouselMotor
+        });
+    }
+
+    public static HardwareComponent[] getDriveTrainMotors(Robot robot){
+
+        MotorConfiguration MC = new MotorConfiguration(
+                MotorType.GO_BUILD_A_YELLOW_JACKET,
+                true,
+                2.9528,
+                1);
+
+        DrivetrainMotor motorFrontLeft = new DrivetrainMotor(robot, "FLM", MC, false, MotorType.DrivetrainPosition.FLM);
+        DrivetrainMotor motorFrontRight = new DrivetrainMotor(robot, "FRM", MC, false, MotorType.DrivetrainPosition.FRM);
+        DrivetrainMotor motorBackLeft = new DrivetrainMotor(robot, "BLM", MC, false, MotorType.DrivetrainPosition.BLM);
+        DrivetrainMotor motorBackRight = new DrivetrainMotor(robot, "BRM", MC, false, MotorType.DrivetrainPosition.BRM);
+
+        return(new HardwareComponent[]{
+                motorFrontLeft,
+                motorFrontRight,
+                motorBackLeft,
+                motorBackRight
+        });
     }
 }
