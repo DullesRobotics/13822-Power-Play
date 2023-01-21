47e21da7d1aa78447dde6aac90b810dd313d73e8
IzackW <izackwaczko@gmail.com>
2023-01-21 01:28:16 -0600

    Rebuilt Config+ Lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fenrir/ControlCenterTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fenrir/ControlCenterTeleOp.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.RobotManager.Robot;
 @Config
 public class ControlCenterTeleOp {
 
-    public static double liftUpModifier = 0.4, liftDownModifier = -0.4, liftDefaultPower = 0.05;
+    public static double liftUpModifier = 0.6, liftDownModifier = -0.4, liftDefaultPower = 0.1;
 
     public static void intakeUpDown(Robot r, Controller ctrl){
         r.addThread(new Thread(() -> {
@@ -20,13 +20,13 @@ public class ControlCenterTeleOp {
             while(r.op().opModeIsActive()){
                 if(ctrl.leftTrigger() > 0){ //DOWN
                     leftLiftMotor.get().setPower(ctrl.leftTrigger() * liftDownModifier);
-                    rightLiftMotor.get().setPower(ctrl.leftTrigger() * liftDownModifier);
+                    rightLiftMotor.get().setPower(ctrl.leftTrigger() * liftDownModifier * -1);
                 } else if(ctrl.rightTrigger() > 0) { //UP
-                    leftLiftMotor.get().setPower(-ctrl.rightTrigger() * liftUpModifier);
+                    leftLiftMotor.get().setPower(-ctrl.rightTrigger() * liftUpModifier * -1);
                     rightLiftMotor.get().setPower(-ctrl.rightTrigger() * liftUpModifier);
                 } else {
                     leftLiftMotor.get().setPower(liftDefaultPower);
-                    rightLiftMotor.get().setPower(liftDefaultPower);
+                    rightLiftMotor.get().setPower(liftDefaultPower * -1);
                 }
             }
         }), true);
