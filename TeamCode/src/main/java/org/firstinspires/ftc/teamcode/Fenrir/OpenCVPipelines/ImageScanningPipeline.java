60735b33aef3f1f65d9e0f878fde71131b611d34
Coder5761 <abhijit.sudharsan@outlook.com>
2023-02-10 16:55:51 -0600

    Added Image Processing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fenrir/OpenCVPipelines/ImageScanningPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fenrir/OpenCVPipelines/ImageScanningPipeline.java
@@ -7,10 +7,12 @@ import org.firstinspires.ftc.teamcode.Libraries.AddOns.Pipeline;
 import org.firstinspires.ftc.teamcode.Libraries.Logger;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
@@ -19,8 +21,13 @@ import java.util.List;
 @Config
 public class ImageScanningPipeline extends OpenCvPipeline implements Pipeline{
 
+    Mat dst = new Mat();
+
+    MatOfRect ducksFound = new MatOfRect();
+
     private Position thisPosition;
     public Mat processFrame(Mat input){
+        Imgproc.cvtColor(input, dst, Imgproc.COLOR_RGB2GRAY);
         Imgproc.rectangle(input, new Rect(new Point(0,100),new Point(200,EasyOpenCV.VIEWPORT_HEIGHT)),new Scalar(255,255,255));
         return input;
     }
@@ -28,8 +35,20 @@ public class ImageScanningPipeline extends OpenCvPipeline implements Pipeline{
     private Position coneMarker(Mat input){
         Position position;
 
+        double numerator = 0;
+        double denominator = 0;
         for(int x = 0; x < EasyOpenCV.VIEWPORT_WIDTH; x+=2){
-            
+            denominator++;
+        }
+
+        CascadeClassifier duckFinder = new CascadeClassifier();
+        duckFinder.load("~/capstone_image.xml");
+
+        duckFinder.detectMultiScale(input, ducksFound, 1.5);
+
+        Rect[] ducksInCapstone = ducksFound.toArray();
+        for(Rect duck : ducksInCapstone){
+            Imgproc.rectangle(input, duck.tl(), duck.br(),new Scalar(0,255,0));
         }
 
         return position;
